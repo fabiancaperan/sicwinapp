b250616cc7f8f78be1c1d612929225b74b964165
Fabian Capera <fabiancaperan@gmail.com>
2020-10-22 19:07:22 -0500

    Add project files.

---
+++ WinApp/chargeFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinApp
+{
+    public partial class chargeFile : Form
+    {
+        public chargeFile()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog search = new OpenFileDialog();
+
+            if (search.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = search.FileName;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = textBox1.Text,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
