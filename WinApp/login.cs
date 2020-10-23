8db92b521047f88ba2eaf625a029194696559e3f
Fabian Capera <fabiancaperan@gmail.com>
2020-10-22 23:28:18 -0500

    init

---
+++ WinApp/login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinApp
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
