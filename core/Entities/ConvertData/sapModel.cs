958047ad3e4cb47dae3748e6069c0a09930a0aa3
camilo fuentes <cafefusi@gmail.com>
2020-10-26 22:27:50 -0500

    Update sapModel.cs

--- core/Entities/ConvertData/sapModel.cs
+++ core/Entities/ConvertData/sapModel.cs
@@ -9,15 +9,15 @@ namespace core.Entities.ConvertData
     {
         [Display(Name = "Tipo Registro")]
         [MaxLength(2)]
-        public int TipoRegistro { get; set; }
+        public string TipoRegistro { get; set; }
 
         [Display(Name = "Fecha Compra")]
         [MaxLength(8)]
-        public DateTime FechaCompra { get; set; }
+        public string FechaCompra { get; set; }
 
         [Display(Name = "Fecha Compra")]
         [MaxLength(13)]
-        public int Nit { get; set; }
+        public string Nit { get; set; }
 
         [Display(Name = "Nombre Cadena")]
         [MaxLength(30)]
@@ -25,7 +25,7 @@ namespace core.Entities.ConvertData
 
         [Display(Name = "Cod_RTL")]
         [MaxLength(19)]
-        public int Cod_RTL { get; set; }
+        public string Cod_RTL { get; set; }
 
         [Display(Name = "Id_Terminal")]
         [MaxLength(16)]
@@ -34,19 +34,19 @@ namespace core.Entities.ConvertData
         //Ciudad
         [Display(Name = "COD_DANE")]
         [MaxLength(8)]
-        public int COD_DANE { get; set; }
+        public string COD_DANE { get; set; }
 
         [Display(Name = "FechaTran")]
         [MaxLength(8)]
-        public DateTime FechaTran { get; set; }
+        public string FechaTran { get; set; }
 
         [Display(Name = "HoraTran")]
         [MaxLength(6)]
-        public int HoraTran { get; set; }
+        public string HoraTran { get; set; }
 
         [Display(Name = "Fiid_Emisor")]
         [MaxLength(4)]
-        public int Fiid_Emisor { get; set; }
+        public string Fiid_Emisor { get; set; }
 
         [Display(Name = "Abrev_Emisor")]
         [MaxLength(3)]
@@ -54,91 +54,91 @@ namespace core.Entities.ConvertData
 
         [Display(Name = "Num_Tarjeta")]
         [MaxLength(4)]
-        public int Num_Tarjeta { get; set; }
+        public string Num_Tarjeta { get; set; }
 
         [Display(Name = "Tipo_Mensaje")]
         [MaxLength(4)]
-        public char Tipo_Mensaje { get; set; }
+        public string Tipo_Mensaje { get; set; }
 
         [Display(Name = "Cod_Trans")]
         [MaxLength(6)]
-        public int Cod_Trans { get; set; }
+        public string Cod_Trans { get; set; }
 
         [Display(Name = "Num_Secuen")]
         [MaxLength(12)]
-        public int Num_Secuen { get; set; }
+        public string Num_Secuen { get; set; }
 
         [Display(Name = "Valor")]
         [MaxLength(12)]
-        public int Valor { get; set; }
+        public string Valor { get; set; }
 
         [Display(Name = "Comision")]
         [MaxLength(8)]
-        public int Comision { get; set; }
+        public string Comision { get; set; }
 
         [Display(Name = "Retencion")]
         [MaxLength(8)]
-        public int Retencion { get; set; }
+        public string Retencion { get; set; }
 
         [Display(Name = "Propina")]
         [MaxLength(8)]
-        public int Propina { get; set; }
+        public string Propina { get; set; }
 
         [Display(Name = "Num_Autoriza")]
         [MaxLength(6)]
-        public int Num_Autoriza { get; set; }
+        public string Num_Autoriza { get; set; }
 
         [Display(Name = "Nombre_Establ")]
         [MaxLength(19)]
-        public char Nombre_Establ { get; set; }
+        public string Nombre_Establ { get; set; }
 
         [Display(Name = "Responder")]
         [MaxLength(1)]
-        public int Responder { get; set; }
+        public string Responder { get; set; }
 
         [Display(Name = "Cod_Resp")]
         [MaxLength(3)]
-        public int Cod_Resp { get; set; }
+        public string Cod_Resp { get; set; }
 
         [Display(Name = "Adquirida_Por")]
         [MaxLength(1)]
-        public int Adquirida_Por { get; set; }
+        public string Adquirida_Por { get; set; }
 
         [Display(Name = "Adquirida_Para")]
         [MaxLength(1)]
-        public int Adquirida_Para { get; set; }
+        public string Adquirida_Para { get; set; }
 
         [Display(Name = "Fiid_Sponsor")]
         [MaxLength(4)]
-        public int Fiid_Sponsor { get; set; }
+        public string Fiid_Sponsor { get; set; }
 
         [Display(Name = "Iva")]
         [MaxLength(8)]
-        public int Iva {get; set; }
+        public string Iva {get; set; }
 
         [Display(Name = "Id_Fran_Hija")]
         [MaxLength(1)]
-        public int Id_Fran_Hija { get; set; }
+        public string Id_Fran_Hija { get; set; }
 
         [Display(Name = "Filler_Fran_Hija")]
         [MaxLength(2)]
-        public int Filler_Fran_Hija { get; set; }
+        public string Filler_Fran_Hija { get; set; }
 
         [Display(Name = "Valor_Liq_Reteica")]
         [MaxLength(10)]
-        public decimal Valor_Liq_Reteica { get; set; }
+        public string Valor_Liq_Reteica { get; set; }
 
         [Display(Name = "Base_Devol_Iva")]
         [MaxLength(12)]
-        public int Base_Devol_Iva { get; set; }
+        public string Base_Devol_Iva { get; set; }
 
         [Display(Name = "RefUniversal")]
         [MaxLength(23)]
-        public int RefUniversal { get; set; }
+        public string RefUniversal { get; set; }
 
         [Display(Name = "ConvBonos")]
         [MaxLength(4)]
-        public int ConvBonos { get; set; }
+        public string ConvBonos { get; set; }
 
         [Display(Name = "TextoAdicional")]
         [MaxLength(25)]
@@ -146,22 +146,22 @@ namespace core.Entities.ConvertData
 
         [Display(Name = "Convtrack")]
         [MaxLength(5)]
-        public int Convtrack { get; set; }
+        public string Convtrack { get; set; }
 
         [Display(Name = "Imp_Consumo")]
         [MaxLength(12)]
-        public int Imp_Consumo { get; set; }
+        public string Imp_Consumo { get; set; }
 
         [Display(Name = "Modo_Ingreso")]
         [MaxLength(3)]
-        public int Modo_Ingreso { get; set; }
+        public string Modo_Ingreso { get; set; }
 
         [Display(Name = "Bin_Tarjeta")]
         [MaxLength(9)]
-        public int Bin_Tarjeta { get; set; }
+        public string Bin_Tarjeta { get; set; }
 
         [Display(Name = "Filler")]
         [MaxLength(41)]
-        public int Filler { get; set; }
+        public string Filler { get; set; }
     }
 }
