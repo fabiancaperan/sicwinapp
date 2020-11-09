c0de44886b750f875a0b716f82819ac763da8e87
fabiancaperan <fabiancaperan@gmail.com>
2020-11-09 00:14:09 -0500

    add funct main

--- WinApp/Main.cs
+++ WinApp/Main.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using core.UseCase.ConvertData;
+using core.UseCase.DownloadData;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Security;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +18,7 @@ namespace WinApp
         public Main()
         {
             InitializeComponent();
+            checkedListBox1.DataSource = Enum.GetValues(typeof(core.Repository.Types.CommerceType));
         }
 
         private void Main_Load(object sender, EventArgs e)
@@ -28,18 +33,12 @@ namespace WinApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog search = new OpenFileDialog();
+            FolderBrowserDialog search = new FolderBrowserDialog();
 
             if (search.ShowDialog() == DialogResult.OK)
             {
-                string ext = Path.GetExtension(search.FileName);
-                if (ext != "")
-                    MessageBox.Show("El archivo no debe tener extensión");
-                else
-                {
-                    textBox1.Text = search.FileName;
-                    textBox1.Enabled = true;
-                }
+                textBox2.Text = search.SelectedPath;
+                textBox2.Enabled = true;
             }
         }
 
@@ -55,16 +54,45 @@ namespace WinApp
 
         private void button2_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        private void btnChargeFileInput_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog search = new OpenFileDialog();
+
+            if (search.ShowDialog() == DialogResult.OK)
+            {
+                string ext = Path.GetExtension(search.FileName);
+                if (ext != "")
+                    MessageBox.Show("El archivo no debe tener extensión");
+                else
+                {
+                    textBoxInput.Text = search.FileName;
+                    textBoxInput.Enabled = true;
+                }
+            }
+        }
+
+        private void textBox_Input(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCharge_Click(object sender, EventArgs e)
+        {
             try
             {
                 button2.Enabled = false;
                 Cursor = Cursors.WaitCursor; // change cursor to hourglass type
 
 
-                if (textBox1.Text != "")
+                if (textBoxInput.Text != "")
                 {
-                    var ret = new ChargeFile().build(textBox1.Text);
+                    var ret = new ChargeFile().build(textBoxInput.Text);
                     MessageBox.Show("Se ha cargado correctamente");
+                    button3.Enabled = true;
+
                 }
                 else
                 {
@@ -80,6 +108,53 @@ namespace WinApp
                 $"Details:\n\n{ex.StackTrace}");
                 Cursor = Cursors.Arrow; // change cursor to normal type
                 button2.Enabled = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var rute = textBox2.Text;
+                List<core.Repository.Types.CommerceType> subfolder = checkedListBox1.CheckedItems.OfType<core.Repository.Types.CommerceType>().ToList();
+                if (rute != "")
+                {
+                    if (subfolder.Count() != 0)
+                    {
+                        button2.Enabled = false;
+                        Cursor = Cursors.WaitCursor; // change cursor to hourglass type
+                        var res = new DownloadFiles().build(rute, subfolder);
+                        MessageBox.Show("Se ha generado con Éxito");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Debe seleccionar al menos un item");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No a seleccionado una ruta");
+                }
+                Cursor = Cursors.Arrow; // change cursor to normal type
+                button2.Enabled = true;
+
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                $"Details:\n\n{ex.StackTrace}");
+                button2.Enabled = true;
+                Cursor = Cursors.Arrow; // change cursor to normal type
             }
         }
     }
