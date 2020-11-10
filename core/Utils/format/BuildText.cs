2627b962828eda7f0a5291713eccc30220f28abd
fabiancaperan <fabiancaperan@gmail.com>
2020-11-09 22:48:35 -0500

    add file

---
+++ core/Utils/format/BuildText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core.Utils.format
+{
+    public class BuildText
+    {
+        private  List<string> characteres = new List<string>(){"*", "&", "$", "%", "!", "¡", "?", "¿", "|", "+", "~", "^", "{", "}", "[", "]", "¨", "°", "¬", "@", ";", ",", ":", ".", "Ñ", "ñ", "´", "`", "<", ">", "=", "(", ")", "/", "\\",  "#", "'", "\""};
+        public string build(string nombre)
+        {
+            string cadena = nombre;
+
+            if (nombre.Trim() == String.Empty)
+                cadena = "(Sin nombre)";
+            
+                cadena = nombre;
+            cadena = cadena.Replace(" ","_");
+            characteres.ForEach(s => 
+            {
+                cadena = cadena.Replace(s, String.Empty);
+            });
+            return cadena;
+        }
+
+    }
+}
