c2bda953062c365f5596c1a291ce78ddfdf930ff
fabiancaperan <fabiancaperan@gmail.com>
2020-11-22 22:31:21 -0500

    add falabellla

--- core/UseCase/falabella/GenerateFalabellaFile.cs
+++ core/UseCase/falabella/GenerateFalabellaFile.cs
@@ -120,63 +120,18 @@ namespace core.UseCase.Falabella
 
             foreach (var g in lstFilter)
             {
-
                 lstText.Add(BuildStep1(g.ToList(), dat.ToString(), date.ToString(), g.Key.NombreCadena));
+                lstText.Add(BuildStep2(g.ToList(), dat.ToString(), date.ToString(), g.Key.NombreCadena));
+                lstText.Add(BuildStep3(g.ToList(), dat.ToString(), date.ToString(), g.Key.NombreCadena));
             }
 
-            var lst = lstSap
-                       .Join(falabella,
-                              post => post.Cod_RTL.Trim(),
-                              meta => _format.Formato(meta.CODIGO_UNICO.Substring(0, 10), 10, A),
-                              (s, e) => new { s, e })
-                        .Where(j => j.s.Nit.Trim() == Nit &&
-                                    Convert.ToInt32(j.s.Cod_Resp.Substring(0, 3)) > 0 &&
-                                    Convert.ToInt32(j.s.Cod_Resp.Substring(0, 3)) < 9 &&
-                                    j.s.Num_Autoriza != string.Empty)
-                        .Select((l, i) =>
-                        {
-
-                            {
-                                var codigo = l.s.Adquirida_Por + l.s.Adquirida_Para == Dinner ? "09" :
-                                        l.s.Adquirida_Por + l.s.Adquirida_Para == Amex ? "11" : "13";
-                                var cod = codigo == "13" ? "|" : codigo + "|";
-                                var fecComp = l.s.FechaCompra.Substring(0, 8);
-                                var codTrans = l.s.Cod_Trans.Substring(0, 2);
-                                var valor = Convert.ToInt64(l.s.Valor.Substring(0, 10));
-                                total += codTrans == "14" ? -(valor) : valor;
-                                return new StringBuilder()
-                            .Append("650RE")
-                            .Append(l.e.LOCAL_FALABELLA.Substring(1, 3))
-                            .Append(cod)
-                            .Append(i + 1)
-                            .Append("|")
-                            .Append(dat)
-                            .Append("|")
-                            .Append(total)
-                            .Append(".00|RED")
-                            .Append(fecComp)
-                            .Append(l.e.LOCAL_FALABELLA)
-                            .Append(codigo)
-                            .Append("|COP|830070527-1|")
-                            .Append(dat)
-                            .Append("|IC")
-                            .Append(fecComp)
-                            .Append(l.e.LOCAL_FALABELLA)
-                            .Append(codigo + "|")
-                            .Append(total)
-                            .Append(".00|||19|CO|||||||||||FACOLOCREDAR")
-                            .Append(fecComp)
-                            .Append("00000000|FACOLOCREDAR");
-                            }
-
-                        }).ToList();
             var rs = new CommerceModel()
             {
                 Rtl = "",
                 Nit = 1 + Nit,
                 Line = "",
                 CodRtl = new StringBuilder().Append("XREDCO_01_").Append(date).Append(".txt").ToString(),
-                Lst = lst
+                Lst = lstText
             };
             var lstres = new List<CommerceModel> { rs };
             return lstres;
