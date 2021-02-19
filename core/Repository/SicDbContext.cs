d0c4cb6c19221d29fcdaeb494a7a9b3593b559fb
fabiancaperan <fabiancaperan@gmail.com>
2021-02-18 23:23:08 -0500

    add database

---
+++ core/Repository/SicDbContext.cs
@@ -0,0 +1,33 @@
+﻿using core.Entities.MasterData;
+using core.Entities.UserData;
+using Microsoft.EntityFrameworkCore;
+
+namespace core.Repository
+{
+    public class SicDbContext : DbContext
+    {
+        public DbSet<EntidadesModel> EntidadesModel { get; set; }
+
+        public DbSet<FalabellaModel> FalabellaModel { get; set; }
+
+        public DbSet<CnbsModel> CnbsModel { get; set; }
+
+        public DbSet<RedprivadasModel> RedprivadasModel { get; set; }
+
+        public DbSet<ConveniosModel> ConveniosModel { get; set; }
+
+        public DbSet<BinesespModel> BinesespModel { get; set; }
+
+        public DbSet<UserModel> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=sic.db");
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<SapModel>()
+        //        .HasKey(c => new { c.Cod_Trans, c.Cod_Resp, c.FechaCompra,c.Nit, c.HoraTran, c.Num_Autoriza, c.Cod_RTL, c.Num_Secuen,c.Valor });
+        //}
+    }
+
+}
